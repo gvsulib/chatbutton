7fbec5447eacad451555049d8c7e305c05d3ed90
Matthew Reidsma <reidsmam@gvsu.edu>
2015-08-06 12:28:29 -0400

    First commit

---
+++ chatbutton.js
@@ -0,0 +1,17 @@
+(function() {
+								var li = document.createElement('li');
+								li.id = 'library-chat';
+								var libChatDiv = document.createElement('a');
+								libChatDiv.appendChild(document.createTextNode('Ask a Question'));
+								libChatDiv.className = 'btn btn-default';
+								li.appendChild(libChatDiv);
+								document.getElementById('cms-navigation').getElementsByTagName('ul')[0].appendChild(li);
+								var chatWindow = function() {
+									window.open('http://labs.library.gvsu.edu/labs/chat', 'Ask a Question', 'menubar=yes,location=yes,resizable=yes,scrollbars=yes,left=20,top=20,width=460,height=460');
+								}
+								if (libChatDiv.addEventListener) {
+									libChatDiv.addEventListener('click', chatWindow, false);
+								} else {
+									libChatDiv.attachEvent('onclick', chatWindow);
+								}
+							})();
