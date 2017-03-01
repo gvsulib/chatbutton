6afd99856aafb234aa14484e474c19ecf827eb62
Matthew Reidsma <reidsmam@gvsu.edu>
2017-03-01 15:22:27 -0500

    Added GET request for statistics gathering

--- chatbutton.js
+++ chatbutton.js
@@ -1,4 +1,7 @@
 (function() {
+	if(typeof chatSystem == "undefined") {
+		var chatSystem = 'CMS';
+	}
 	var li = document.createElement('li');
 	li.id = 'library-chat';
 	var libChatDiv = document.createElement('a');
@@ -7,7 +10,7 @@
 	li.appendChild(libChatDiv);
 	document.getElementById('cms-navigation').getElementsByTagName('ul')[0].appendChild(li);
 	var chatWindow = function() {
-		window.open('https://prod.library.gvsu.edu/labs/chat', 'Ask a Question', 'menubar=yes,location=yes,resizable=yes,scrollbars=yes,left=20,top=20,width=460,height=460');
+		window.open('https://prod.library.gvsu.edu/labs/chat' + chatSystem, 'Ask a Question', 'menubar=yes,location=yes,resizable=yes,scrollbars=yes,left=20,top=20,width=460,height=460');
 	}
 	if (libChatDiv.addEventListener) {
 		libChatDiv.addEventListener('click', chatWindow, false);
