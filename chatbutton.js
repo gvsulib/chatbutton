bbffa502cf40bd20f8706fb3e2a47aebb4269244
Matthew Reidsma <reidsmam@gvsu.edu>
2020-03-25 21:06:46 -0400

    Redirected chat button since Engineering servers are down again

--- chatbutton.js
+++ chatbutton.js
@@ -8,8 +8,13 @@
 	libChatDiv.className = 'btn btn-default';
 	li.appendChild(libChatDiv);
 	document.getElementById('cms-navigation').getElementsByTagName('ul')[0].appendChild(li);
+	/*
 	var chatWindow = function() {
 		window.open('https://prod.library.gvsu.edu/labs/chat/?' + chatReferrer, 'Ask a Question', 'menubar=yes,location=yes,resizable=yes,scrollbars=yes,left=20,top=20,width=460,height=460');
+	}
+	*/
+	var chatWindow = function() {
+		window.open('https://v2.libanswers.com/widget_chat.php?hash=6290b3d40228a9e708fa7066d01f56bf', 'Ask a Question', 'menubar=yes,location=yes,resizable=yes,scrollbars=yes,left=20,top=20,width=460,height=460');
 	}
 	if (libChatDiv.addEventListener) {
 		libChatDiv.addEventListener('click', chatWindow, false);
